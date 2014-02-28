53deeeea50dd0bc8dabbf1aefb41942fef383d0f
pse <pse@ableton.com>
2014-02-28 18:44:06 +0100

    Add rudimentary unit test

---
+++ sort_imports_tests.py
@@ -0,0 +1,34 @@
+from textwrap import dedent
+from unittest import TestCase
+
+from sort_imports import main
+
+class TestSortImports(TestCase):
+
+    def test(self):
+        input_ = """from __future__ import absolute_import
+from spam import eggs
+import sys
+from wsgiref.handlers import CGIHandler, BaseCGIHandler, SimpleHandler
+from ast import Expr, parse, Import, ImportFrom, NodeTransformer, NodeVisitor, Module
+from ..base import spam, eggs, foobar
+from .. import bla
+"""
+        expected_output = """from __future__ import absolute_import
+import sys
+from ast import (
+    Expr,
+    Import,
+    ImportFrom,
+    Module,
+    NodeTransformer,
+    NodeVisitor,
+    parse
+)
+from wsgiref.handlers import BaseCGIHandler, CGIHandler, SimpleHandler
+from spam import eggs
+from ..base import eggs, foobar, spam
+from .. import bla"""
+        res = main(input_,
+                   ["ast", "wsgiref", "spam"])
+        self.assertEqual(expected_output, res)
